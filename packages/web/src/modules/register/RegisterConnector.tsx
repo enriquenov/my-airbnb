32f85a62450ca0ad29569fb18b206577490ab8ac
enrique novoa <luisenriquenovoa@gmail.com>
2019-05-05 21:52:55 -0700

    Initialized the controller package and abstracted the submit logic from web.

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { RegisterController } from '@my-airbnb/controller';
 import { RegisterView } from './view/RegisterView';
 
 // container -> view (presentational)
@@ -6,12 +7,11 @@ import { RegisterView } from './view/RegisterView';
 // controller -> connector -> view
 
 export class RegisterConnector extends React.PureComponent {
-  dummySubmit = async (values: any) => {
-    console.log(values);
-    return null;
-  };
-
   render() {
-    return <RegisterView submit={this.dummySubmit} />;
+    return (
+      <RegisterController>
+        {({ submit }) => <RegisterView submit={submit} />}
+      </RegisterController>
+    );
   }
 }
